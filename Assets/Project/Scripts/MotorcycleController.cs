f43f98337c8e316abf93e1c8a7a2a21e0dcaac8e
PhilipeFernandino <philipegarciaf@gmail.com>
2024-02-23 21:39:07 -0300

    feat: moto tilt, front yaw, model improved

--- Assets/Project/Scripts/MotorcycleController.cs
+++ Assets/Project/Scripts/MotorcycleController.cs
@@ -1,37 +1,43 @@
 using NaughtyAttributes;
+using System;
+using UnityEditor;
 using UnityEngine;
 
 public class MotorcycleController : MonoBehaviour
 {
-    [SerializeField] private Rigidbody _rigidbody;
-
+    [Header("Properties")]
     [SerializeField] private float _maxSpeed = 5f;
     [SerializeField] private float _acceleration = 2f;
     [SerializeField] private float _steeringSpeed = 1f;
     [SerializeField] private float _rearWheelDistance = 2f;
 
-    [SerializeField] private Transform _frontRaycastTransform;
-    [SerializeField] private Transform _rearRaycastTransform;
     [SerializeField] private float _groundRaycastDistance = 1f;
+    [SerializeField] private float _isGroundedRaycastDistance = 0.5f;
+    [SerializeField] private float _adjustRotationScale = 15f;
 
     [SerializeField] private LayerMask _groundLayerMask;
+    
+    [Header("Object References")]
+    [SerializeField] private Rigidbody _rigidbody;
+
+    [SerializeField] private Transform _frontRaycastTransform;
+    [SerializeField] private Transform _rearRaycastTransform;
+
     [SerializeField] private Transform _bikeMesh;
+    [SerializeField] private Transform _frontMeshGroup;
 
     private Vector3 _originalRotation;
     private Vector3 _originalPosition;
+    private Vector3 _localVelocity;
 
     private float _x;
     private float _y;
-    private bool _isBreaking;
-
-    private float _currentSpeed;
-
-    private float _frontGroundDistance;
-    private float _rearGroundDistance;
 
     private bool _frontRaycastHit;
     private bool _rearRaycastHit;
+
     private float _slopeAngle;
+    private bool _isGrounded;
 
     private void Awake()
     {
@@ -46,63 +52,78 @@ public class MotorcycleController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        _rigidbody.velocity = Vector3.Lerp(_rigidbody.velocity, _maxSpeed * _y * _rigidbody.transform.forward, Time.fixedDeltaTime * _acceleration);
-        _rigidbody.angularVelocity = Vector3.zero; 
-        transform.Rotate(0, _x * Time.fixedDeltaTime * _steeringSpeed, 0);
+        _localVelocity = transform.InverseTransformDirection(_rigidbody.velocity);
+        _rigidbody.angularVelocity = Vector3.zero;
+
+        int hasForwardVelocity = Utils.Math.Near(_rigidbody.velocity.magnitude, 0f, 0.1f) ? 0 : 1;
+        int velocitySign = (int) Mathf.Sign(_localVelocity.z);
+
+        _isGrounded = IsGrounded();
+
+        if (_isGrounded)
+        {
+            _rigidbody.velocity = Vector3.Lerp(_rigidbody.velocity, _maxSpeed * _y * _rigidbody.transform.forward, Time.fixedDeltaTime * _acceleration);
+
+            _bikeMesh.Rotate(0, 0, 10 * -_x * hasForwardVelocity);
+            _frontMeshGroup.localEulerAngles = new Vector3(-90f, _x * 25, 0);
+        }
+
+        transform.Rotate(0, _x * hasForwardVelocity * velocitySign * Time.fixedDeltaTime * _steeringSpeed, 0);
+
         RaycastSlope();
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(_frontRaycastTransform.position, -Vector3.up, _isGroundedRaycastDistance, _groundLayerMask)
+            || Physics.Raycast(_rearRaycastTransform.position, -Vector3.up, _isGroundedRaycastDistance, _groundLayerMask);
     }
 
     private void RaycastSlope()
     {
-        Vector3 raycastDir = -Vector3.up * _groundRaycastDistance;
 
         if (Physics.Raycast(_frontRaycastTransform.position, -Vector3.up, out RaycastHit frontHit, _groundRaycastDistance, _groundLayerMask))
         {
             _frontRaycastHit = true;
-            _frontGroundDistance = frontHit.distance;
-            Debug.DrawRay(_frontRaycastTransform.position, raycastDir, Color.green);
         }
         else
         {
-            Debug.DrawRay(_frontRaycastTransform.position, raycastDir, Color.red);
             _frontRaycastHit = false;
         }
 
         if (Physics.Raycast(_rearRaycastTransform.position, -Vector3.up, out RaycastHit rearHit, _groundRaycastDistance, _groundLayerMask))
         {
             _rearRaycastHit = true;
-            _rearGroundDistance = rearHit.distance;
-            Debug.DrawRay(_rearRaycastTransform.position, raycastDir, Color.green);
         }
         else
         {
             _frontRaycastHit = false;
-            Debug.DrawRay(_rearRaycastTransform.position, raycastDir, Color.red);
         }
 
-        if (!_frontRaycastHit || !_rearRaycastHit)
-        {
-            return;
-        }
 
-        _slopeAngle = Vector3.SignedAngle(frontHit.point - rearHit.point, transform.forward, transform.right);//Mathf.Rad2Deg * (Mathf.Atan2(frontHit.point.y - rearHit.point.y, frontHit.point.z - rearHit.point.z));
-
+        DrawRaycast(_frontRaycastTransform.position, _frontRaycastHit, _groundRaycastDistance);
+        DrawRaycast(_rearRaycastTransform.position, _rearRaycastHit, _groundRaycastDistance);
         Debug.DrawLine(
-            frontHit.point,
-            rearHit.point);
+        frontHit.point,
+        rearHit.point);
 
-        Vector3 eulerAngles = _rigidbody.transform.eulerAngles;
-        eulerAngles.x = -_slopeAngle;
-        _bikeMesh.eulerAngles = eulerAngles;
+        if (_frontRaycastHit && _rearRaycastHit)
+        {
+            _slopeAngle = Vector3.SignedAngle(frontHit.point - rearHit.point, transform.forward, transform.right);
+
+            var rotation = _rigidbody.transform.rotation;
+            rotation.eulerAngles = new(-_slopeAngle, rotation.eulerAngles.y, rotation.eulerAngles.z);
+            _bikeMesh.rotation = Quaternion.Lerp(_bikeMesh.rotation, rotation, _adjustRotationScale * Time.fixedDeltaTime);
+        }
     }
 
     private void GetInput()
     {
         _y = Input.GetAxis("Vertical");
         _x = Input.GetAxis("Horizontal");
-        _isBreaking = Input.GetKey(KeyCode.Space);
     }
 
+    #region Debug
     [Button]
     private void ResetAll()
     {
@@ -115,9 +136,25 @@ public class MotorcycleController : MonoBehaviour
     private void OnGUI()
     {
         GUILayout.BeginArea(new Rect(new Vector2(10, 10), new Vector2(400, 400)));
-        GUILayout.TextArea($"Front Wheel V: {_rigidbody.velocity}\n" +
-            $"Front Rotation: {_rigidbody.rotation}\n" +
-            $"Slope Angle: {_slopeAngle}");
+        GUILayout.TextArea($"Velocity: {_rigidbody.velocity}\n" +
+            $"Local Velocity: {_localVelocity}\n" +
+            $"Rotation: {_rigidbody.rotation}\n" +
+            $"Slope Angle: {_slopeAngle}\n" +
+            $"Is Grouned: {_isGrounded}");
         GUILayout.EndArea();
     }
+
+    private void DrawRaycast(Vector3 position, bool hit, float distance)
+    {
+        Vector3 raycastDir = -Vector3.up * distance;
+        if (hit)
+        {
+            Debug.DrawRay(position, raycastDir, Color.green);
+        }
+        else
+        {
+            Debug.DrawRay(position, raycastDir, Color.red);
+        }
+    }
+    #endregion
 }
